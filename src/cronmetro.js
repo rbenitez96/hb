b143dc213a8f7bf5425907cf3ee1420f5e5e1902
ANGEL\ronny <rbenitez@espol.edu.ec>
2023-05-14 09:37:37 -0500

    Clue 1

--- src/cronmetro.js
+++ src/cronmetro.js
@@ -1,5 +1,6 @@
 // Define la fecha del cumpleaños (YYYY-MM-DD)
 const birthday = '2023-05-31';
+const video = document.querySelector('#cuadro1 video');
 
 // Calcula el tiempo restante para el cumpleaños
 function countdown() {
@@ -16,7 +17,7 @@ function countdown() {
     document.getElementById('countdown').innerHTML = days + ' días, ' + hours + ' horas, ' + minutes + ' minutos, y ' + seconds + ' segundos';
 
     // Actualiza el estado de los botones de pista
-    if (days <= 10) {
+    if (days <= 15) {
         document.getElementById('pista1').disabled = false;
     }
     if (days <= 5) {
@@ -26,6 +27,33 @@ function countdown() {
         document.getElementById('pista3').disabled = false;
     }
 }
+
+function ocultarCuadroPistas(){
+    document.getElementById('cuadro-pistas').style.display = 'none';
+}
+
+function mostrarCuadroPistas(){
+    document.getElementById('cuadro-pistas').style.display = 'flex';
+}
+
+function ocultarVideo() {
+    // Ocultar el contenedor del video
+    document.getElementById('cuadro1').style.display = 'none';
+    video.stop();
+    ocultarCuadroPistas();
+}
+
+
+// Función para desbloquear el cuadro de video
+function desbloquearVideo() {
+    mostrarCuadroPistas();
+    document.getElementById('cuadro1').style.display = 'block';
+    document.getElementById('pista1').disabled = true;
+    video.play();
+    video.addEventListener('ended', ocultarVideo);
+}
+
+document.getElementById('pista1').addEventListener('click', desbloquearVideo);
 
 // Actualiza el cronómetro cada segundo
 setInterval(countdown, 1000);
